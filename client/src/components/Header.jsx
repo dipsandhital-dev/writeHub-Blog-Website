9aaf201d8717cf3d34b1d4f37121364547737a0a
Dipsan Dhital <dipsandhital12@gmail.com>
2026-01-31 19:22:29 +0545

    Changing Ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,37 @@
-
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="relative mx-8  ms:mx-16 xl:mx-24">
+      <div className="text-center mt-10 mb-8">
+        {/* ! Badge */}
+        <div className="inline-flex items-center justify-center gap-3 px-6 py-1.5  mb-4 border border-primary/40 bg-primary/10 rounded-full text-sm text-primary ">
+          New: AI Features Integrated
+        </div>
 
-export default Header
+        <h1 className="text-4xl sm:text-6xl font-semibold text-gray-700 sm:leading-tight ">
+          Your own <span className="text-primary"> blogging</span> <br />
+          platform.
+        </h1>
+
+        <p className="my-6 sm:my-8 max-w-2xl mx-auto text-gray-500 max-sm-text-xs ">
+          This is your space to think out loud, to share what matters, and to
+          write without filters. Whether it’s one word or a thousand, your story
+          starts right here.
+        </p>
+
+
+        {/*  Search  */}
+        <form  className="flex justify-between max-w-lg mx-auto bg-white border border-gray-300 rounded  overflow-hidden shadow-sm max-sm:scale-75">
+          <input type=" text" placeholder="Search for blog" required className="w-full pl-4 outline-none text-sm" />
+
+          <button className="bg-primary text-white px-8 py-2 m-1.5 rounded transition-transformation duration-200 hover:scale-105">
+            Search
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
